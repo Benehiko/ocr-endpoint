a280057f4c8d7b16a62d0e5f2ae0bb0430310ebf
Alano Terblanche <alanoterblanche@gmail.com>
2018-10-22 21:36:37 +0200

    setup password

--- src/main/java/com/benehiko/Setup.java
+++ src/main/java/com/benehiko/Setup.java
@@ -13,8 +13,8 @@ public class Setup {
 
     @Bean
     public AcsApplication createApplication() {
-        return new AcsApplicationBuilder().withIpAddress("192.168.122.5").withUsername("root").withPassword("Iknow").build(); //.withPassword("tL^f8XEF").build();
-        //return new AcsApplicationBuilder().withIpAddress("localhost").withUsername("root").withPassword("tL^f8XEF").build();
+        //return new AcsApplicationBuilder().withIpAddress("192.168.122.5").withUsername("root").withPassword("Iknow").build(); //.withPassword("tL^f8XEF").build();
+        return new AcsApplicationBuilder().withIpAddress("localhost").withUsername("root").withPassword("tL^f8XEF").build();
     }
 
     @Bean
